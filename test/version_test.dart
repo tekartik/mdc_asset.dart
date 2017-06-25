b3ef471d17357bf58c97df06a9e52cc5cac3ecda
Alexandre Roux Tekartik <alex@tekartik.com>
2017-06-25 16:03:59 +0200

    test: version test is on vm only

--- test/version_test.dart
+++ test/version_test.dart
@@ -1,9 +1,9 @@
 // Copyright (c) 2017, alex. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
+@TestOn("vm")
 import 'dart:io';
 import 'package:path/path.dart';
 import 'package:tekartik_common_utils/version_utils.dart';
-@TestOn("vm")
 import 'package:tekartik_pub/io.dart';
 import 'package:test/test.dart';
 import 'package:yaml/yaml.dart';
