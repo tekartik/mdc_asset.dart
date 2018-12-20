3136fddf446dfbe5701d92b47d59d4132be7b339
Alexandre Roux <alex@tekartik.com>
2018-12-20 20:45:18 +0100

    feat: mdc 0.42.1

--- tool/install.dart
+++ tool/install.dart
@@ -2,10 +2,11 @@ import 'dart:io';
 import 'package:fs_shim/utils/io/copy.dart';
 import 'package:path/path.dart';
 import 'package:tekartik_build_utils/cmd_run.dart';
+import 'package:tekartik_build_utils/node/node_cmd.dart';
 
+/// To run to update the css and js
 main() async {
-  await runCmd(
-      ProcessCmd("npm", ["install", "--save", "material-components-web"]));
+  await runCmd(NpmCmd(["install", "--save", "material-components-web"]));
   await copyDirectory(
       new Directory(join("node_modules", "material-components-web", "dist")),
       new Directory("lib"));
